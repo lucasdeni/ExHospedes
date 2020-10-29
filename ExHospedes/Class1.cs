b729c4783ee008b2c263d9b5b2dd74ce63970e24
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-28 22:31:21 -0300

    classe pronta

---
+++ ExHospedes/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExHospedes
+{
+    class Hospedes
+    {
+        public string Nome { get; set; } //Get e Set dos Nomes dos hóspedes
+        public string Email { get; set; } //Get e Set dos Emails dos Hóspedes
+        public Hospedes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
