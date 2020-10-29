346e0d96f12232ddff7eaeb6f7b5f0d5243ae7e2
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-28 22:42:58 -0300

    comecei o programa

--- ExHospedes/Program.cs
+++ ExHospedes/Program.cs
@@ -6,7 +6,25 @@ namespace ExHospedes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hospedes[] vect = new Hospedes[10];
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aluguel # {i}: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quartos: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Hospedes(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
         }
     }
 }
