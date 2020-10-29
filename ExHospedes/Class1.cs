0b324e30a08bd7c9bab12b25f609765c817087ee
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-28 22:32:52 -0300

    classe finalizada

--- ExHospedes/Class1.cs
+++ ExHospedes/Class1.cs
@@ -8,12 +8,12 @@ namespace ExHospedes
     {
         public string Nome { get; set; } //Get e Set dos Nomes dos hóspedes
         public string Email { get; set; } //Get e Set dos Emails dos Hóspedes
-        public Hospedes(string nome, string email)
+        public Hospedes(string nome, string email) //Construtor personalizado
         {
             Nome = nome;
             Email = email;
         }
-        public override string ToString()
+        public override string ToString() //Declaração da String
         {
             return Nome + ", " + Email;
         }
